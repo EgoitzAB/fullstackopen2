a51f83adfb1710e5e470379ebd14abaec369ada8
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-03-06 21:37:02 +0100

    part3.6

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    
     const newPerson = {
         id: generateId(),
         name: body.name,
