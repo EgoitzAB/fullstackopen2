74e3d50e3c4a691bb1744e9056f13fe7910b7400
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-03-06 21:34:50 +0100

    part3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+
 let persons = [
     { id: 1, name: "Arto Hellas", number: "040-123456" },
     { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
@@ -38,6 +40,29 @@ app.get('/info', (req, res) => {
          <p>${timestamp}</p>`
     );
 });
+
+const generateId = () => {
+    const finalId = Math.floor(Math.random() * 10000000000000000000)
+    return finalId
+  }
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const newPerson = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+}
+)
 
 const PORT = 3001;
 app.listen(PORT, () => {
