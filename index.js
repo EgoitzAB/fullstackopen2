f918ea58e28571b3a71b5d992832be3906ebc7cc
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-03-08 00:29:31 +0100

    part3.9

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :remote-addr :status :res[content-length] - :response-time ms :body :date[iso]'))
