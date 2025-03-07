4b5411b68f47650f31abe313558b26da6d4b349b
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-03-08 00:53:05 +0100

    part3.11

---
+++ README.md
@@ -0,0 +1,5 @@
+# Phonebook Backend
+
+Este es el backend de la Agenda Telefónica, desplegado en Render.
+
+🌍 **URL del backend:** [https://fullstackopen2-r3mb.onrender.com/api/persons](https://fullstackopen2-r3mb.onrender.com/api/persons)
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :remote-addr :status :res[content-length] - :response-time ms :body :date[iso]'))
