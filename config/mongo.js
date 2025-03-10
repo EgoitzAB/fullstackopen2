5def8a86cb45ad3d54eb2f298ff894e1fa24877f
Egoitz Abilleira <egoitzabilleira@protonmail.com>
2025-03-10 23:52:29 +0100

    part3.14 done, console.log exposing data deleted

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
 const MONGODB_URI = process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
